43f3775d7237b29f13ec2873894c4d76274d1145
waltherbaasch <U2444127@rts-365.dk>
2023-04-20 10:32:57 +0200

    added js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 
 import './App.css'
+import HeadingOne from './components/HeadingOne'
 
 function App() {
   const [count, setCount] = useState(0)
+  return(
+    <div className="App">
+    <header> 
+      <HeadingOne />
+      </header>
+      <main>
+<section></section>
+<section></section>
+      </main>
+      <footer>
+        <nav></nav>
+      </footer>
+      </div>
+  );
 
 }
 
